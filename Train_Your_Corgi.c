a23c348ddbe938e2527966e71b1ff7554d1fb8d2
chiu-chiu-kuan <devaraja1945@gmail.com>
2022-10-15 20:11:09 +0800

    the final one and it got ac

--- Train_Your_Corgi.c
+++ Train_Your_Corgi.c
@@ -28,28 +28,28 @@ int main(void)
     //initizal the strength and obedience
     Strength strength =INIT_STRENGTH;
     Obedience obedience =INIT_OBEDIENCE;
-     int n=0;//成功的指令數
+    int n=0;//成功的指令數
     //get the input
-   while(1){
-    int cmd =getchar();//讀取一個字元
-   
-    if(strength<=LEARN_STRENGTH&&obedience>=LEARN_OBEDIENCE)//當服從度不小於 LEARN_OBEDIENCE，並且體力值不大於 LEARN_STRENGTH 時，就可以成功訓練一個指令。
-    {
-        n+=1;//成功一次了
-        obedience=INIT_OBEDIENCE;//這時候柯基的體力值不變，但服從度會變為 INIT_OBEDIENCE。
-        //printf("hahaah\n");
-    }
-    
-    //eating 當小智餵食柯基時，會增加 STRENGTH_EAT 量的體力值，但會降低 OBEDIENCE_EAT 的服從度
-     if(cmd=='e')
+    while(1){
+        int cmd =getchar();//讀取一個字元
+
+        if(strength<=LEARN_STRENGTH&&obedience>=LEARN_OBEDIENCE)//當服從度不小於 LEARN_OBEDIENCE，並且體力值不大於 LEARN_STRENGTH 時，就可以成功訓練一個指令。
         {
-        strength+=STRENGTH_EAT;
-        obedience-=OBEDIENCE_EAT;
+            n+=1;//成功一次了
+            obedience=INIT_OBEDIENCE;//這時候柯基的體力值不變，但服從度會變為 INIT_OBEDIENCE。
+            //printf("hahaah\n");
+        }
+
+        //eating 當小智餵食柯基時，會增加 STRENGTH_EAT 量的體力值，但會降低 OBEDIENCE_EAT 的服從度
+        if(cmd=='e')
+        {
+            strength+=STRENGTH_EAT;
+            obedience-=OBEDIENCE_EAT;
         }
         else if(cmd=='t')//while traing 當柯基接受訓練時，會增加 OBEDIENCE_TRAIN 量的服從度，並減少 STRENGTH_TRAIN 的體力值
         {
-        strength-=STRENGTH_TRAIN;
-        obedience+=OBEDIENCE_TRAIN;
+            strength-=STRENGTH_TRAIN;
+            obedience+=OBEDIENCE_TRAIN;
         }
         else if (cmd=='\n')//the input end and it's time to see the ans（結算）
         {
@@ -65,11 +65,11 @@ int main(void)
                 return 0;
             }
         }
-   }
-    
+    }
 
-   
-   
+
+
+
 
     //return 0;
 }
